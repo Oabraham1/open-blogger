9a45774459de78a8f9de3f88af0dad74b491837c
Oabraham1 <abrahamojima2018@gmail.com>
2023-06-19 15:51:11 -0400

    feat: started auth

---
+++ server/middleware/auth.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func IsAuthenticated(ctx *gin.Context) {
+	if sessions.Default(ctx).Get("profile") == nil {
+		ctx.Redirect(http.StatusSeeOther, "/")
+	} else {
+		ctx.Next()
+	}
+}
